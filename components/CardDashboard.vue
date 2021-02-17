ba1f8ad5bb35675e8a58158de00a928b0797ef38
bigilisandro <lisandro.bigi@icloud.com>
2021-02-17 20:44:08 -0300

    design dashboard page

---
+++ components/CardDashboard.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <div class="card">
+      <div :style="{ 'background-image': bgImage }" class="bgImage">
+        <div
+          class="is-flex is-align-items-flex-end is-justify-content-flex-end is-fullheight"
+        >
+          <b-tag type="is-primary">{{ model.title }}</b-tag>
+        </div>
+        <!-- <figure class="image">
+          <img :src="model.image" :alt="model.title" class="border-green" />
+        </figure> -->
+        <!-- <div v-show="hover" class="hoverImage">
+          <b-button rounded @click="viewModel">View</b-button>
+        </div> -->
+      </div>
+    </div>
+    <div class="card-content">
+      <div class="content">
+        <h4 class="title has-text-primary">
+          {{ model.title }} <span class="mx-2">|</span> $ {{ model.price }}
+        </h4>
+        <p class="subtitle is-7">{{ model.description }}</p>
+        <div class="is-flex is-justify-content-space-around">
+          <a class="is-flex is-align-items-center">
+            <img
+              src="@/assets/images/icon_pdf.svg"
+              alt="icon_pdf"
+              class="image is-32x32 mr-2"
+            />
+            <p class="subtitle is-7">Download Specs</p>
+          </a>
+          <a class="is-flex is-align-items-center">
+            <img
+              src="@/assets/images/icon_share.svg"
+              alt="icon_share"
+              class="image is-32x32 mr-2"
+            />
+            <p class="subtitle is-7">Share</p>
+          </a>
+          <a class="is-flex is-align-items-center">
+            <img
+              src="@/assets/images/icon_edit.svg"
+              alt="icon_edit"
+              class="image is-32x32 mr-2"
+            />
+            <p class="subtitle is-7">Edit Home</p>
+          </a>
+        </div>
+        <div class="is-flex mt-4 is-align-items-center">
+          <div>
+            <b-button rounded size="is-small">SEND TO S2A</b-button>
+          </div>
+          <div>
+            <p class="subtitle is-7 ml-3">
+              A representative will reach out to you.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    model: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      bgImage: 'url(' + this.model.image + ')',
+    }
+  },
+}
+</script>
+
+<style scoped>
+.border-green {
+  border-bottom: 5px solid #027881;
+}
+.bgImage {
+  height: 300px;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  border-bottom: 5px solid #027881;
+}
+</style>
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <section>
+    <div class="bg-home py-6">
+      <h1 class="title is-2 has-text-centered my-6">ELIZABETH'S HOMES</h1>
+      <!-- HOMES -->
+      <div class="container is-fluid">
+        <div class="columns is-multiline">
+          <div
+            v-for="model in models"
+            :key="model.id"
+            class="column is-4"
+            @mouseover="showByIndex = model"
+            @mouseout="showByIndex = null"
+          >
+            <card-dashboard
+              :hover="showByIndex === model"
+              :model="model"
+              @view-model="viewModel(model.id)"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import CardDashboard from '@/components/CardDashboard.vue'
+import model1 from '@/assets/images/model-1.png'
+import model2 from '@/assets/images/model-2.png'
+import model3 from '@/assets/images/model-3.png'
+
+export default {
+  name: 'HomePage',
+  components: {
+    CardDashboard,
+  },
+  data() {
+    return {
+      showByIndex: null,
+      signUpModal: false,
+      models: [
+        {
+          id: 1,
+          image: model1,
+          title: 'MODEL 1',
+          price: '731,500',
+          description:
+            '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        },
+        {
+          id: 2,
+          image: model2,
+          title: 'MODEL 6',
+          price: '515,000',
+          description:
+            '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        },
+        {
+          id: 3,
+          image: model3,
+          title: 'MODEL 11',
+          price: '246,800',
+          description:
+            '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        },
+        {
+          id: 4,
+          image: model1,
+          title: 'MODEL 4',
+          price: '731,900',
+          description:
+            '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        },
+      ],
+    }
+  },
+  methods: {
+    viewModel(idModel) {
+      this.$router.push({ name: 'model-id', params: { id: idModel } })
+    },
+  },
+}
+</script>
+
+<style scoped>
+.bg-home {
+  background-image: url(~assets/images/background.png);
+  background-size: cover;
+}
+</style>
