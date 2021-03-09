fd1af2c84e148eba78f96e20f8e758dc9704fbe7
bigilisandro <lisandro.bigi@icloud.com>
2021-03-09 16:48:39 -0300

    test refresh dynamic pages 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,10 @@ export default {
     '@nuxtjs/pwa',
     '@nuxtjs/auth-next',
   ],
-
+  generate: {
+    fallback: true,
+    crawler: false,
+  },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     baseURL: process.env.API_BASE_URL,
